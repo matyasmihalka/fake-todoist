4689c84a39542f0ec21b707c0529b5845795a155
Matyas Mihalka <matyas.mihalka@applifting.cz>
2024-02-26 10:13:54 +0000

    feat: editable tidi items

--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -2,8 +2,13 @@ import { Checkbox } from './Checkbox'
 import { useTimezoneStore, useToDoStore } from '@/store'
 import dayjs from '@/dayjsConfig'
 import { Dayjs } from 'dayjs'
+import { ToDo } from '@/store/toDoStore'
 
-export const ToDoList = () => {
+export const ToDoList = ({
+  openModal,
+}: {
+  openModal: (toDo: ToDo) => void
+}) => {
   const toDoList = useToDoStore((state) => state.toDoList)
   const checkToDo = useToDoStore((state) => state.checkToDo)
   const activeTimezone = useTimezoneStore((state) => state.activeTimezone)
@@ -26,6 +31,14 @@ export const ToDoList = () => {
     return formattedDate.replace('T', ' ')
   }
 
+  const openTodo = (id: string) => {
+    const toDo = toDoList.find((item) => item.id === id)
+    if (!toDo) {
+      throw new Error('ToDo not found')
+    }
+    openModal(toDo)
+  }
+
   return (
     <section>
       <h2>To do</h2>
@@ -34,7 +47,13 @@ export const ToDoList = () => {
           <li key={index} className="border-b flex py-2 gap-2 hover:bg-gray-50">
             <Checkbox checked={checked} onChange={handleCheckboxChange(id)} />
             <div className="">
-              <div className="text-base pb-1">{title}</div>
+              <div
+                className="text-base pb-1"
+                style={{ cursor: 'pointer' }}
+                onClick={() => openTodo(id)}
+              >
+                {title}
+              </div>
               <div className="text-xs text-gray-500">{description}</div>
               <div className="text-xs text-rose-500">
                 {toggleTimezone(date)}
